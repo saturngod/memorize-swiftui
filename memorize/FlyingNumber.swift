3fa5312cdf0fcec00195fa4d26e0c07be4f0ce09
Htain Lin Shwe <saturngod@gmail.com>
2023-09-18 18:32:03 -0500

    add deck

---
+++ memorize/FlyingNumber.swift
@@ -0,0 +1,20 @@
+//
+//  FlyingNumber.swift
+//  memorize
+//
+//  Created by Htain Lin Shwe on 12/09/2023.
+//
+
+import SwiftUI
+
+struct FlyingNumber: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FlyingNumber_Previews: PreviewProvider {
+    static var previews: some View {
+        FlyingNumber()
+    }
+}
